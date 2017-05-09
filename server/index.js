b10f03d82290ce6cc40c4f2ee25995e430f018d8
Ravi Jaisawal <ravi.jaisawal@bookmyshow.com>
2017-05-09 13:30:57 +0530

    Build a ToDo App Using React, Redux, and Webpack

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const volleyball = require('volleyball');
+app.use(volleyball);
+//serve up static files
+app.use(express.static(path.resolve(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '..', 'node_modules')));
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
